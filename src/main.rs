f27c399130866e241a3afc2535f2e9fff089bd2c
Nicholas R. Smith <nicholasrsmith1600@gmail.com>
2023-11-04 17:44:59 -0700

    Fix lint error

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ const APP_PORT: u16 = 8000;
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let listener = TcpListener::bind(format!("localhost:{}", APP_PORT))
-        .expect(&format!("Failed to bind to port {}", APP_PORT));
+        .unwrap_or_else(|_| panic!("Failed to bind to port {}", APP_PORT));
 
     run(listener)?.await
 }
