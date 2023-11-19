2c88ba8156490abcce1ae7aa0e46e978a898660a
Nicholas R. Smith <nicholasrsmith1600@gmail.com>
2023-11-18 22:18:41 -0800

    Fix syntax error and use form for /subscriptions POST request

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use actix_web::{dev::Server, web, App, HttpResponse, HttpServer};
 use std::net::TcpListener;
 
-#[derive(serde:Deserialize)]
+#[derive(serde::Deserialize)]
 struct FormData {
     email: String,
     name: String,
@@ -11,7 +11,7 @@ async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
-async fn subscribe() -> HttpResponse {
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
